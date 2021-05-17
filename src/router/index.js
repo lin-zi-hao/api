c376947272a2ab4137827ced19cc093f49c481bc
18029714387 <2285051456@qq.com>
2021-05-17 16:52:27 +0800

    初始化

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Router from "vue-router";
+import Vue from "vue";
+
+Vue.use(Router);
+
+const routes = [];
+
+const router = new Router({
+  routes,
+  mode:"history"
+})
+
+export default router
